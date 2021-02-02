7bd32e86c15593ce3465dbfcda0982d0ebaefa13
Samuel Colvin <s@muelcolvin.com>
2021-02-02 21:27:37 +0000

    add python 3.9 pypi classifier

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Intended Audience :: Developers',
         'Intended Audience :: Information Technology',
         'Intended Audience :: System Administrators',

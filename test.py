350f70f9f574faa5e3e0e857edf78a219e9b7102
thomas eeckhout <thomas.eeckhout@aca-it.be>
2023-02-08 17:58:47 +0100

    added testfile can be ignored

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import asyncio
+import contextlib
 from aioaws.s3 import S3Client, S3Config
 from httpx import AsyncClient
+from aioaws._utils import pretty_xml
 
 
 async def s3_demo(client: AsyncClient):
@@ -14,11 +16,29 @@ async def s3_demo(client: AsyncClient):
             "localhost:9000/testaioaws",
         ),
     )
+
+    await s3.delete('path/testt.txt')
+
     async with s3.createMultipartUpload('path/testt.txt') as mpu:
         a = "123456" * 1_000_000
         b = "7890"
         await mpu.uploadPart(1, a)
-        await mpu.uploadPart(2, b)
+        await mpu.uploadPart(3, a)
+
+        await mpu.uploadPart(2, a)
+
+        x = await mpu.listParts(1000)
+        print(x)
+        print("--")
+
+    async with s3.createMultipartUpload('path/testt.txt') as mpu:
+        x = await mpu.listParts(1000)
+        print(x)
+        print("--")
+
+    async for x in s3.list():
+        print(x)
+
 
 async def main():
     async with AsyncClient(timeout=30) as client:
